1cd9c51d924d8fd78230636dd779d248f79b0a10
jiashuang <jias.tw@gmail.com>
2017-03-28 11:50:47 +0800

    refine timeout

--- findVOD.js
+++ findVOD.js
@@ -1,19 +1,25 @@
 
 var gateways = ['192.168.1.1', '192.168.0.1'];
-var gateways_errors = 0;
-var scan_errors = 0;
 var vod_path = '/vod/';
 var vod_png = 'mic-icon.png';
 var vod_lastIPNumMin = 2;
 var vod_lastIPNumMax = 254;
+var found = 0;
+var fails = 0;
 
-function msg(s) {
+function log(msg) {
     var text = $('#result').html();
-    text += s;
+    text += '<p>'+msg+'</p>';
     $('#result').html(text);
 }
 
-function ping(ip, callback) {
+function addLink(link) {
+    var text = $('#result').html();
+    text += '<h1><a href=\"http://'+link+'\">'+link+'</a></h1>\n';
+    $('#result').html(text);
+}
+
+function ping(ip, callback, timeout = 0) {
 
     if (!this.inUse) {
         this.status = 'unchecked';
@@ -30,62 +36,63 @@ function ping(ip, callback) {
         this.img.onerror = function (e) {
             if (_that.inUse) {
                 _that.inUse = false;
-                console.log(e);
                 _that.callback(_that.ip, 'error');
             }
 
         };
         this.start = new Date().getTime();
-        this.img.src = 'http://' + ip;
-        this.timer = setTimeout(function () {
-            if (_that.inUse) {
-                _that.inUse = false;
-                _that.callback(_that.ip, 'timeout');
-            }
-        }, 1500);
+        this.img.src = 'http://' + ip + '?'+ new Date().getTime();
+        if (timeout) {
+            this.timer = setTimeout(function () {
+                if (_that.inUse) {
+                    _that.inUse = false;
+                    _that.img.src = '';
+                    _that.callback(_that.ip, 'timeout');
+                }
+            }, timeout);
+        }
     }
 }
 
 function redirect(ip) {
     var re = new RegExp(vod_png);
     var vod = ip.replace(re, '');
-    localStorage.setItem('vod', vod);
-    window.location = 'http://'+vod;
+    //localStorage.setItem('vod', vod);
+    addLink(vod);
+    found = found + 1;
 }
 
 function onScanResult(ip, status) {
     if (status == 'load') {
         redirect(ip);
     } else {
-        scan_errors = scan_errors + 1;
-        if (scan_errors == (vod_lastIPNumMax - vod_lastIPNumMin + 1)) {
-           msg('ERROR: No vod\n');
+        fails = fails + 1;
+        console.log([ip, status, fails.toString()].join(','));
+        if (fails == (vod_lastIPNumMax - vod_lastIPNumMin + 1 - found)) {
+            log('Found : '+found.toString());
         }
     }
 }
 
 function scanVOD(ip) {
-    ip_nums = ip.split('.');
+    log('scanning '+ip+'/255.255.255.0');
+    var ip_nums = ip.split('.');
+    var prefix = ip_nums[0] + '.' + ip_nums[1] + '.' + ip_nums[2] + '.';
     for (var i = vod_lastIPNumMin; i <= vod_lastIPNumMax; i++) {
-        var addr = ip_nums[0] + '.' + ip_nums[1] + '.' + ip_nums[2] + '.' + i.toString();
-        new ping(addr+vod_path+vod_png, onScanResult);
+        new ping(prefix+i.toString()+vod_path+vod_png, onScanResult);
     }
+
 }
 
 function onGatewayResult(ip, status) {
     if (status != 'timeout') {
         scanVOD(ip);
-    } else {
-        gateways_errors = gateways_errors + 1;
-        if (gateways_errors == gateways.length) {
-            msg('ERROR: No gateway\n');
-        }
     }
 }
 
 function findGateway() {
     for (var i=0; i<gateways.length; i++) {
-        new ping(gateways[i], onGatewayResult);
+        new ping(gateways[i], onGatewayResult, 1000);
     }
 }
 
