05f2063ca55d668cea1808ecfe088f278490e11e
jias_huang <jias_huang@realtek.com>
2017-03-29 11:22:43 +0800

    init -3
    :Release Notes:
    
    :Detailed Notes:
    - Problem :
    - RootCause :
    - Solution :
    - Author :
    - Build :
    
    :Testing Preformed:
    How to Test :

--- findVOD.js
+++ findVOD.js
@@ -1,13 +1,27 @@
 
-var gateways = ['192.168.1.1', '192.168.0.1'];
+var vod_gateways = '192.168.1.1, 192.168.0.1';
 var vod_path = '/vod/';
-var vod_png = 'mic-icon.png';
+var vod_image = 'mic-icon.png';
 var vod_lastIPNumMin = 2;
 var vod_lastIPNumMax = 254;
+var vod_auto = null;
 var found = 0;
 var fails = 0;
 var start = false;
 var redirect = false;
+
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
+function extractDIR(link, dir) {
+    var re = new RegExp('(.*)'+escapeRegExp(dir));
+    var match = link.match(re);
+    if (match) {
+        return match[0];
+    }
+    return null;
+}
 
 function log(msg) {
     var text = $('#log').html();
@@ -25,6 +39,9 @@ function updateProgress() {
     var percent = Math.floor((fails + found) * 100 / (vod_lastIPNumMax - vod_lastIPNumMin + 1));
     $('#progress').html('Progress: '+percent.toString()+'%');
     if (percent == 100) {
+        if (!found) {
+            log('Oops! Not Found');
+        }
         start = false;
         $('#start').removeClass('hl');
     }
@@ -36,13 +53,13 @@ function onImageError(element) {
 }
 
 function onImageLoad(element) {
-    var vod = element.src.match(/(.*)vod\//)[0];
+    var vod = extractDIR(element.src, vod_path)
     if (redirect) {
         localStorage.setItem('vod', vod);
         window.location = vod;
         return;
     }
-    addLink(vod[0]);
+    addLink(vod);
     found = found + 1;
     $('#found').html('Found: '+found.toString());
     updateProgress();
@@ -54,7 +71,7 @@ function scanVOD(ip) {
     var prefix = ip_nums[0] + '.' + ip_nums[1] + '.' + ip_nums[2] + '.';
     var text = '';
     for (var i = vod_lastIPNumMin; i <= vod_lastIPNumMax; i++) {
-        var link = prefix + i.toString() + vod_path + vod_png + '?' + new Date().getTime();
+        var link = prefix + i.toString() + vod_path + vod_image + '?' + new Date().getTime();
         text += '<img src="http://'+link+'" onload="onImageLoad(this)" onerror="onImageError(this)" \>';
     }
     $('#images').html(text);
@@ -104,13 +121,15 @@ function onGatewayResult(ip, status) {
 }
 
 function findGateway() {
-    for (var i=0; i<gateways.length; i++) {
-        new ping(gateways[i], onGatewayResult, 1500);
+    var lists = vod_gateways.replace(/\s/g, '').split(',');
+    for (var i=0; i<lists.length; i++) {
+        new ping(lists[i], onGatewayResult, 1500);
     }
 }
 
 function findVOD() {
     if (!start) {
+        save();
         start = true;
         found = fails = 0;
         $('#log').html('');
@@ -119,11 +138,32 @@ function findVOD() {
     }
 }
 
+function save() {
+    var lists = ['vod_gateways', 'vod_path', 'vod_image'];
+    for (var i=0; i<lists.length; i++) {
+        this[lists[i]] = document.getElementById(lists[i]).value;
+        localStorage.setItem(lists[i], this[lists[i]]);
+    }
+ }
+
+function onInit() {
+    var lists = ['vod_gateways', 'vod_path', 'vod_image'];
+    for (var i=0; i<lists.length; i++) {
+        var x = localStorage.getItem(lists[i]);
+        if (x && x.length > 0) {
+            this[lists[i]] = x;
+        }
+    }
+    for (var i=0; i<lists.length; i++) {
+        document.getElementById(lists[i]).value = this[lists[i]];
+    }
+}
+
 function onAuto() {
+    onInit();
     redirect = true;
-    var vod = localStorage.getItem('vod');
-    if (vod && vod.length > 0) {
-        var link = vod + vod_png + '?' + new Date().getTime();
+    if (vod_auto) {
+        var link = vod_auto + vod_image + '?' + new Date().getTime();
         var text = '<img src="'+link+'" onload="onImageLoad(this)" onerror="findVOD()" \>';
         $('#images').html(text);
     } else {
